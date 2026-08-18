b72296ec27b032fb698fdd7a6d3de30396d786ae
YoungKudi <youngkudi3000@gmail.com>
2026-08-18 01:17:11 +0000

    chore: trigger deployment after password fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,3 +7,4 @@ export default async function Home() {
   return session ? redirect('/dashboard') : redirect('/login')
 }
 // Trigger deployment Mon Aug 17 22:37:41 GMT 2026
+// Trigger deployment Tue Aug 18 01:17:11 GMT 2026
